4297d8a4e604586fb054e67c7a44b557d1946a17
Jester565 <ajcraig543@gmail.com>
2017-02-16 00:31:51 -0800

    Updated email_confirm.html

--- NodeSite/NodeSite/public/javascripts/email_confirm.js
+++ NodeSite/NodeSite/public/javascripts/email_confirm.js
@@ -1,83 +1,53 @@
 ﻿"use strict";
 
-function setErrorMsg(str) {
-  $('#msg').removeClass('hidden');
-  $('#msg').text(str);
-  $('#confirmButton').addClass('error');
-  $('#confirmButton').removeClass('load');
-  $('#confirmButton h2').text('ERROR');
-}
-
 function EmailConfirmManager(root) {
-  this.PackI0 = root.lookup("ProtobufPackets.PackI0");
-  this.PackI1 = root.lookup("ProtobufPackets.PackI1");
-  this.PackI2 = root.lookup("ProtobufPackets.PackI2");
+		emailConfirmManager = this;
+  emailConfirmManager.PacketB2 = root.lookup("ProtobufPackets.PackB2");
+  emailConfirmManager.PacketB3 = root.lookup("ProtobufPackets.PackB3");
 
-  client.packetManager.addPKey(new PKey("I2", function (iPack) {
+  client.packetManager.addPKey(new PKey("B3", function (iPack) {
     $('#loading').addClass('hidden');
-    var packI2 = emailConfirmManager.PackI2.decode(iPack.packData);
-    if (packI2.success) {
+    var packB3 = emailConfirmManager.PacketB3.decode(iPack.packData);
+    if (packB3.success) {
       console.log("Success!");
-      $('#login').addClass('hidden');
       $('#checkmark').removeClass('hidden');
       $('#msg').removeClass('hidden');
       $('#msg').addClass('success');
-      $('#msg').text(packI2.msg);
+      $('#msg').text(packB3.msg);
     }
     else {
-      if (packI2.requestI1) {
-        $('#login').removeClass('hidden');
-      }
-      setErrorMsg(packI2.msg);
+      setErrorMsg(packB3.msg);
     }
-  }, this, "Gets the success of the login"));
+  }, emailConfirmManager, "Gets if the confirmation was successful"));
+
+	 emailConfirmManager.setErrorMsg = function (str) {
+				$('#msg').removeClass('hidden');
+				$('#msg').text(str);
+		};
+
+		var url = window.location.href;
+  var questionI = url.indexOf('?');
+  if (questionI !== -1) {
+    emailConfirmManager.emailToken = url.substring(++questionI);
+				var packB2 = emailConfirmManager.PacketB2.create({
+						emailToken: emailConfirmManager.emailToken
+				});
+				client.tcpConnection.sendPack(new OPacket("B2", true, [0], packB2, emailConfirmManager.PacketB2));
+		} else {
+				emailConfirmManager.setErrorMsg("No email token in url");
+		}
 }
 
 var emailConfirmManager;
 
 var client = new Client(function (root) {
-  emailConfirmManager = new EmailConfirmManager(root);
-  client.tcpConnection.onopen = function () {
-    var url = window.location.href;
-    var questionI = url.indexOf('?');
-    if (questionI !== -1) {
-      emailConfirmManager.emailToken = url.substring(++questionI);
-      var creationToken = Cookies.get('creationToken');
-      console.log("CreationToken: " + creationToken);
-      if (creationToken !== undefined) {
-        var packI0 = emailConfirmManager.PackI0.create({ emailToken: emailConfirmManager.emailToken, creationToken: creationToken });
-        console.log(packI0.creationToken);
-        client.tcpConnection.sendPack(new OPacket("I0", true, [0], packI0, emailConfirmManager.PackI0));
-      }
-      else {
-        $('#confirmButton').click(function () {
-          if ($('#userName').val().length === 0) {
-            setErrorMsg('Name not entered');
-          }
-          else if ($('#pwd').val().length === 0) {
-            setErrorMsg('Password not entered');
-          }
-          else {
-            $('#confirmButton').addClass('load');
-            var packI1 = emailConfirmManager.PackI1.create({ emailToken: emailConfirmManager.emailToken, userName: $('#userName').val(), pwd: $('#pwd').val() });
-            client.tcpConnection.sendPack(new OPacket("I1", true, [0], packI1, emailConfirmManager.PackI1));
-          }
-        });
-
-        $('#userName, #pwd').focus(function () {
-          if ($('#confirmButton').hasClass('error')) {
-            $('#confirmButton').removeClass('error');
-            $('#msg').addClass('hidden');
-            $('#confirmButton h2').text('CONFIRM');
-          }
-        });
-        $('#loading').addClass('hidden');
-        $('#login').removeClass('hidden');
-      }
-    }
-    else {
-      $('#loading').addClass('hidden');
-      setErrorMsg('Invalid URL: No token');
-    }
+  console.log("ON LOAD CALLED");
+  innerLoginManager = new InnerLoginManager(client, root,
+				function () {
+						console.log("LOGGED IN");
+						emailManager = new EmailConfirmManager(client.root);
+				});
+  client.tcpConnection.onclose = function () {
+    window.location = './noServer.html';
   };
 });
