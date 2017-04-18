af774553fea68dfa0fe21660e1072ba4b1cad8b2
Jester565 <ajcraig543@gmail.com>
2017-04-18 12:45:48 -0700

    Added allow-control-origin to express

--- NodeSite/NodeSite/app.js
+++ NodeSite/NodeSite/app.js
@@ -35,6 +35,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use('/', function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow_Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb' }));
--- NodeSite/NodeSite/appTest.js
+++ NodeSite/NodeSite/appTest.js
@@ -32,6 +32,11 @@ var credentials = {
 var express = require('express');
 var app = express();
 
+app.use('/', function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow_Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb' }));
