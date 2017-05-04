204100843da0cc1f0845d406b8de44c909052bdd
Jester565 <ajcraig543@gmail.com>
2017-05-04 10:59:15 -0700

    Using application/octet-stream

--- NodeSite/NodeSite/public/javascripts/resume.js
+++ NodeSite/NodeSite/public/javascripts/resume.js
@@ -218,7 +218,7 @@ function ResumeManager() {
 		resumeManager.pdf = null;
 		resumeManager.pdfIndex = 0;
 		$('#pdfDownload').click(function () {
-			var pdfBlob = new Blob([resumeManager.pdfData.buffer], { type: "application/pdf;charset=utf-8" });
+			var pdfBlob = new Blob([resumeManager.pdfData.buffer], { type: "application/octet-stream" });
 			saveAs(pdfBlob, $('#pdfTitle').text());
 		});
 		$('#pdfExit').click(function () {
