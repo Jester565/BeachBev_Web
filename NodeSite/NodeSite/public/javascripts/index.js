cc2c4dd50a251bafbf1957575d0c6e5a94a8bec2
Jester565 <ajcraig543@gmail.com>
2017-05-03 22:56:14 -0700

    index uses javascript to scale well across resolutions

--- NodeSite/NodeSite/public/javascripts/index.js
+++ NodeSite/NodeSite/public/javascripts/index.js
@@ -3,6 +3,7 @@
 
 var setman = null;
 var homeManager = null;
+
 $(document).ready(function () {
 	$("#mBar").load("./mBar.html", function () {
 		homeManager = new HomeManager();
@@ -17,5 +18,17 @@ function HomeManager() {
 		$('#ordernow').click(function () {
 			Redirect("menu.html");
 		});
+		homeManager.setBeachDivFontSize();
+		$(window).resize(homeManager.setBeachDivFontSize);
+	}
+
+
+	this.setBeachDivFontSize = function () {
+		var fontSize = $('#homeDiv').width() / 35.0;
+		$("#stepDiv").css("margin-top", String(fontSize * 1.4 - 15) + 'px');
+		$("#stepDiv").css("margin-right", String(fontSize) + 'px');
+		$("#stepDiv").css("fontSize", String(fontSize * 1.2) + 'px');
+		$("#step4").css("fontSize", String(fontSize * 3) + 'px');
+		$('#step4').css("margin-top", '-' + String((fontSize * 3) * 1.5) + 'px');
 	}
 }
